7e7bc6e497787861eefb22c7260e4f818ec1e7a1
Praveen Kumar <praveenscience@gmail.com>
2019-09-01 13:31:57 +0100

    Updated the welcome message & created routes.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const { GenerateJWT, DecodeJWT, ValidateJWT } = require("./dec-enc.js");
+
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3100;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const welcomeMessage =
+  "Welcome to the API Home Page. Please look at the documentation to learn how to use this web service.";
+
+app.get("/", (req, res) => res.send(welcomeMessage));
+
+app.post("/api/GenerateJWT", (req, res) => res.send(""));
+app.post("/api/DecodeJWT", (req, res) => res.send(""));
+app.post("/api/ValidateJWT", (req, res) => res.send(""));
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
