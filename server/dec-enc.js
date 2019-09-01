93fbcb5bad0b584a8fd74ef21a29c7da4b6656bf
Praveen Kumar <praveenscience@gmail.com>
2019-09-01 13:27:21 +0100

    Reorganised procedural code to functional code.

--- server/dec-enc.js
+++ server/dec-enc.js
@@ -1,10 +1,26 @@
 const JSRSASign = require("jsrsasign");
 
-const GenerateJWT = () => {};
+const GenerateJWT = (header, claims, key) => {
+  // Let's convert everything into strings.
+  const sHeader = JSON.stringify(header);
+  const sPayload = JSON.stringify(claims);
+  // Generate the JWT and return it to the caller.
+  const sJWT = JSRSASign.jws.JWS.sign("HS512", sHeader, sPayload, key);
+  return sJWT;
+};
 
-const DecodeJWT = () => {};
+const DecodeJWT = sJWS => {
+  const aJWT = sJWS.split(".");
+  const uHeader = JSRSASign.b64utos(aJWT[0]);
+  const uClaim = JSRSASign.b64utos(aJWT[1]);
+  const pHeader = JSRSASign.jws.JWS.readSafeJSONString(uHeader);
+  const pClaim = JSRSASign.jws.JWS.readSafeJSONString(uClaim);
+  return pClaim;
+};
 
-const ValidateJWT = () => {};
+const ValidateJWT = (header, token, key) => {
+  return JSRSASign.jws.JWS.verifyJWT(token, key, header);
+};
 
 module.exports = {
   GenerateJWT,
