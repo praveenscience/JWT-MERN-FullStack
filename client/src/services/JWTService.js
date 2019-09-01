6fd8fb31747ef44b2caf29ba3106e9449ce5c898
Praveen Kumar <praveenscience@gmail.com>
2019-09-01 18:10:19 +0100

    Used axios to send POST request to all services and call cb.

--- client/src/services/JWTService.js
+++ client/src/services/JWTService.js
@@ -1,11 +1,45 @@
 import axios from "axios";
 
 export const GenerateJWT = (header, claims, key, cb) => {
-  // Send request to /api/GenerateJWT
+  // Send POST request to /api/GenerateJWT
+  axios
+    .post("/api/GenerateJWT", {
+      header,
+      claims,
+      key
+    })
+    .then(function(res) {
+      cb(res);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 };
 export const DecodeJWT = (sJWS, cb) => {
-  // Send request to /api/DecodeJWT
+  // Send POST request to /api/DecodeJWT
+  axios
+    .post("/api/DecodeJWT", {
+      sJWS
+    })
+    .then(function(res) {
+      cb(res);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 };
 export const ValidateJWT = (header, token, key, cb) => {
-  // Send request to /api/ValidateJWT
+  // Send POST request to /api/ValidateJWT
+  axios
+    .post("/api/ValidateJWT", {
+      header,
+      token,
+      key
+    })
+    .then(function(res) {
+      cb(res);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 };
