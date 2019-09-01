5a0f8e1c59186d53adff51e88a997c1eac0dda38
Praveen Kumar <praveenscience@gmail.com>
2019-09-01 13:24:14 +0100

    Synced the complete code.

--- server/dec-enc.js
+++ server/dec-enc.js
@@ -1,45 +1,45 @@
 const JSRSASign = require("jsrsasign");
 
+// Generation
 const claims = {
   Username: "praveen",
   Age: 27,
   Fullname: "Praveen Kumar"
 };
-
 const key = "$PraveenIsAwesome!";
-
 const header = {
   alg: "HS512",
   typ: "JWT"
 };
 
-// Stringify the header and payload objects.
 const sHeader = JSON.stringify(header);
 const sPayload = JSON.stringify(claims);
-
-// Generate and log the JWT.
+// Generate the JWT
 const sJWT = JSRSASign.jws.JWS.sign("HS512", sHeader, sPayload, key);
-console.log(sJWT);
+// Log it to the console.
+console.log("JSON Web Token: ", sJWT);
 
-// Validating the JWT.
 const token =
   "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJVc2VybmFtZSI6InByYXZlZW4iLCJBZ2UiOjI3LCJGdWxsbmFtZSI6IlByYXZlZW4gS3VtYXIifQ.Nut6I57FYUGP973LgfOUNUBjMlaIm6NKk8ffgX4BTdQ_Wc2ob8m6uOwWlgoNMxpuRQaOoeFQOHhrIOJ_V8E-YA";
 const algorithm = "HS512";
 
-// Output of Validation.
+// Log it to the console.
 console.log(
+  "Verification: ",
+  // Validation
   JSRSASign.jws.JWS.verifyJWT(token, key, {
     alg: [algorithm]
   })
 );
 
-// Decoding function.
-const aJWT = token.split(".");
+// Decoding
+const sJWS = token;
+const aJWT = sJWS.split(".");
 const uHeader = JSRSASign.b64utos(aJWT[0]);
 const uClaim = JSRSASign.b64utos(aJWT[1]);
-
-// Parse and log the untrusted Header and Claim.
 const pHeader = JSRSASign.jws.JWS.readSafeJSONString(uHeader);
 const pClaim = JSRSASign.jws.JWS.readSafeJSONString(uClaim);
-console.log(pHeader);
-console.log(pClaim);
+// Decoded objects.
+// Log it to the console.
+console.log("Header: ", pHeader);
+console.log("Claim: ", pClaim);
