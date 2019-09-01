f6e5106dfc0bf6953271884205bb7154a0aebae8
Praveen Kumar <praveenscience@gmail.com>
2019-09-01 13:25:43 +0100

    Removed all console logs.

--- server/dec-enc.js
+++ server/dec-enc.js
@@ -16,21 +16,10 @@ const sHeader = JSON.stringify(header);
 const sPayload = JSON.stringify(claims);
 // Generate the JWT
 const sJWT = JSRSASign.jws.JWS.sign("HS512", sHeader, sPayload, key);
-// Log it to the console.
-console.log("JSON Web Token: ", sJWT);
 
 const token =
   "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJVc2VybmFtZSI6InByYXZlZW4iLCJBZ2UiOjI3LCJGdWxsbmFtZSI6IlByYXZlZW4gS3VtYXIifQ.Nut6I57FYUGP973LgfOUNUBjMlaIm6NKk8ffgX4BTdQ_Wc2ob8m6uOwWlgoNMxpuRQaOoeFQOHhrIOJ_V8E-YA";
 const algorithm = "HS512";
-
-// Log it to the console.
-console.log(
-  "Verification: ",
-  // Validation
-  JSRSASign.jws.JWS.verifyJWT(token, key, {
-    alg: [algorithm]
-  })
-);
 
 // Decoding
 const sJWS = token;
@@ -39,7 +28,3 @@ const uHeader = JSRSASign.b64utos(aJWT[0]);
 const uClaim = JSRSASign.b64utos(aJWT[1]);
 const pHeader = JSRSASign.jws.JWS.readSafeJSONString(uHeader);
 const pClaim = JSRSASign.jws.JWS.readSafeJSONString(uClaim);
-// Decoded objects.
-// Log it to the console.
-console.log("Header: ", pHeader);
-console.log("Claim: ", pClaim);
